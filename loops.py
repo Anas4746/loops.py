b2cea9cf6cdb3b0635d00fb0461044d2de03cd18
huzefa979 <huzefa979@gmail.com>
2022-12-28 21:04:48 +0530

    Loops in python

---
+++ loops.py
@@ -0,0 +1,19 @@
+x = 1
+z = 1
+while x <= 5:
+    print("Anas", end="")
+    x = x+1
+
+while z <= 5:
+    print("khan", end="")
+    z = z + 1
+    x = 1
+    print()
+    x = 1
+    while x <= 5:
+        print("Anas", end="")
+        x = x + 1
+        print("khan", end="")
+
+
+
